b03ce675a2ba34e0073cbbad27cccc62a2e589b6
Artem Boiko <artem.boiko@badal.io>
2022-10-19 07:47:30 -0400

    Make storing test results optional

--- macros/store_test_results.sql
+++ macros/store_test_results.sql
@@ -25,97 +25,99 @@ https://github.com/brooklyn-data/dbt_artifacts/blob/781a8f8d028dbb2e88daa1c8bbc8
 {% endmacro %}
 
 {% macro store_test_results(results) %}
-  {%- set test_results = [] -%}
+  {% if var('enable_store_test_results', False) == True -%}
+    {%- set test_results = [] -%}
 
-  {%- for result in results if result.node.resource_type == 'test' -%}
-    {%- set test_results = test_results.append(result) -%}
-  {%- endfor -%}
+    {%- for result in results if result.node.resource_type == 'test' -%}
+      {%- set test_results = test_results.append(result) -%}
+    {%- endfor -%}
 
-  {% if test_results|length == 0 -%}
-    {{ log("store_test_results found no test results to process.") if execute }}
-    {{ return('') }}
-  {% endif -%}
+    {% if test_results|length == 0 -%}
+      {{ log("store_test_results found no test results to process.") if execute }}
+      {{ return('') }}
+    {% endif -%}
 
-  {%- set central_tbl -%} {{ target.schema }}.test_results_central {%- endset -%}
-  {%- set history_tbl -%} {{ target.schema }}.test_results_history {%- endset -%}
+    {%- set central_tbl -%} {{ target.schema }}.test_results_central {%- endset -%}
+    {%- set history_tbl -%} {{ target.schema }}.test_results_history {%- endset -%}
 
-  {{ log("Centralizing " ~ test_results|length ~ " test results in " + central_tbl, info = true) if execute }}
-  {{ log(test_results, info=true) }}
-  create or replace table {{ central_tbl }} as (
+    {{ log("Centralizing " ~ test_results|length ~ " test results in " + central_tbl, info = true) if execute }}
+    {{ log(test_results, info=true) }}
+    create or replace table {{ central_tbl }} as (
 
-  {%- for result in test_results %}
+    {%- for result in test_results %}
 
-    {%- set test_name = '' -%}
-    {%- set test_type = '' -%}
-    {%- set column_name = '' -%}
+      {%- set test_name = '' -%}
+      {%- set test_type = '' -%}
+      {%- set column_name = '' -%}
 
-    {%- if result.node.test_metadata is defined -%}
-      {%- set test_name = result.node.test_metadata.name -%}
-      {%- set test_type = 'generic' -%}
+      {%- if result.node.test_metadata is defined -%}
+        {%- set test_name = result.node.test_metadata.name -%}
+        {%- set test_type = 'generic' -%}
 
-      {%- if test_name == 'relationships' -%}
-        {%- set column_name = result.node.test_metadata.kwargs.field ~ ',' ~ result.node.test_metadata.kwargs.column_name -%}
-      {%- else -%}
-        {%- set column_name = result.node.test_metadata.kwargs.column_name -%}
-      {%- endif -%}
-    {%- elif result.node.name is defined -%}
-      {%- set test_name = result.node.name -%}
-      {%- set test_type = 'singular' -%}
-    {%- endif %}
-
-    select
-      '{{ test_name }}' as test_name,
-      '{{ result.node.config.severity }}' as test_severity_config,
-      '{{ result.status }}' as test_result,
-      '{{ dbt_store_test_results.process_refs(result.node.refs) }}' as model_refs,
-      '{{ dbt_store_test_results.process_refs(result.node.sources, is_src=true) }}' as source_refs,
-      '{{ column_name|escape }}' as column_names,
-      '{{ result.node.name }}' as test_name_long,
-      '{{ test_type }}' as test_type,
-      '{{ result.execution_time }}' as execution_time_seconds,
-      '{{ result.node.original_file_path }}' as file_test_defined,
-      '{{ var("pipeline_name", "variable_not_set") }}' as pipeline_name,
-      '{{ var("pipeline_type", "variable_not_set") }}' as pipeline_type,
-      '{{ target.name }}' as dbt_cloud_target_name,
-      ARRAY<string>{{ result.node.tags }} as tags,
-      current_timestamp as _timestamp,
-      {{ 0 if result.failures is none else result.failures }} as rows_failed,
-      {%- if result.node.refs|length == 1 -%}
-        {%- if result.node.config.where -%}
-          (SELECT count(*) FROM {{ dbt_store_test_results.get_relation(result.node.refs[0][0]) }} WHERE {{ result.node.config.where }})
+        {%- if test_name == 'relationships' -%}
+          {%- set column_name = result.node.test_metadata.kwargs.field ~ ',' ~ result.node.test_metadata.kwargs.column_name -%}
         {%- else -%}
-          (SELECT count(*) FROM {{ dbt_store_test_results.get_relation(result.node.refs[0][0]) }})
-        {%- endif %}
-      {%- elif result.node.sources|length == 1 -%}
-        {%- if result.node.config.where -%}
-          (SELECT count(*) FROM {{ source(result.node.sources[0][0], result.node.sources[0][1]) }} WHERE {{ result.node.config.where }})
-        {%- else -%}
-          (SELECT count(*) FROM {{ source(result.node.sources[0][0], result.node.sources[0][1]) }})
-        {%- endif %}
-      {%- else -%}
-          0
-      {%- endif %} as rows_total
-    {{ "union all" if not loop.last }}
+          {%- set column_name = result.node.test_metadata.kwargs.column_name -%}
+        {%- endif -%}
+      {%- elif result.node.name is defined -%}
+        {%- set test_name = result.node.name -%}
+        {%- set test_type = 'singular' -%}
+      {%- endif %}
 
-  {%- endfor %}
-
-  );
-
-  {% if target.name != 'default' %}
-      create table if not exists {{ history_tbl }} as (
-        select
-          {{ dbt_utils.surrogate_key(["test_name", "test_result", "_timestamp"]) }} as sk_id,
-          *
-        from {{ central_tbl }}
-        where false
-      );
-
-    insert into {{ history_tbl }}
       select
-       {{ dbt_utils.surrogate_key(["test_name", "test_result", "_timestamp"]) }} as sk_id,
-       *
-      from {{ central_tbl }}
-    ;
+        '{{ test_name }}' as test_name,
+        '{{ result.node.config.severity }}' as test_severity_config,
+        '{{ result.status }}' as test_result,
+        '{{ dbt_store_test_results.process_refs(result.node.refs) }}' as model_refs,
+        '{{ dbt_store_test_results.process_refs(result.node.sources, is_src=true) }}' as source_refs,
+        '{{ column_name|escape }}' as column_names,
+        '{{ result.node.name }}' as test_name_long,
+        '{{ test_type }}' as test_type,
+        '{{ result.execution_time }}' as execution_time_seconds,
+        '{{ result.node.original_file_path }}' as file_test_defined,
+        '{{ var("pipeline_name", "variable_not_set") }}' as pipeline_name,
+        '{{ var("pipeline_type", "variable_not_set") }}' as pipeline_type,
+        '{{ target.name }}' as dbt_cloud_target_name,
+        ARRAY<string>{{ result.node.tags }} as tags,
+        current_timestamp as _timestamp,
+        {{ 0 if result.failures is none else result.failures }} as rows_failed,
+        {%- if result.node.refs|length == 1 -%}
+          {%- if result.node.config.where -%}
+            (SELECT count(*) FROM {{ dbt_store_test_results.get_relation(result.node.refs[0][0]) }} WHERE {{ result.node.config.where }})
+          {%- else -%}
+            (SELECT count(*) FROM {{ dbt_store_test_results.get_relation(result.node.refs[0][0]) }})
+          {%- endif %}
+        {%- elif result.node.sources|length == 1 -%}
+          {%- if result.node.config.where -%}
+            (SELECT count(*) FROM {{ source(result.node.sources[0][0], result.node.sources[0][1]) }} WHERE {{ result.node.config.where }})
+          {%- else -%}
+            (SELECT count(*) FROM {{ source(result.node.sources[0][0], result.node.sources[0][1]) }})
+          {%- endif %}
+        {%- else -%}
+            0
+        {%- endif %} as rows_total
+      {{ "union all" if not loop.last }}
+
+    {%- endfor %}
+
+    );
+
+    {% if target.name != 'default' %}
+        create table if not exists {{ history_tbl }} as (
+          select
+            {{ dbt_utils.surrogate_key(["test_name", "test_result", "_timestamp"]) }} as sk_id,
+            *
+          from {{ central_tbl }}
+          where false
+        );
+
+      insert into {{ history_tbl }}
+        select
+        {{ dbt_utils.surrogate_key(["test_name", "test_result", "_timestamp"]) }} as sk_id,
+        *
+        from {{ central_tbl }}
+      ;
+    {% endif %}
   {% endif %}
 
 {% endmacro %}
